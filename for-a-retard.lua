ec27b6f57fbcb1cf1fb711dca73fb2b126996deb
osplurm <admin@benjamina.xyz>
2024-09-26 00:51:50 -0500

    Create for-a-retard.lua

---
+++ for-a-retard.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JackyPoopoo/cartel/main/0000000000000000000000000000000000000000000000000"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/osplurm/scripts/main/micup.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shadow62x/catbypass/main/upfix"))()
